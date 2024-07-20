c80acb9f51b0297a19186b00660039e7b86470d7
gravelBridge <john.tian31@gmail.com>
2024-07-19 23:17:29 -0700

    cate

---
+++ categorization/categorize.py
@@ -0,0 +1,42 @@
+from openai import OpenAI
+import json
+from dotenv import load_dotenv
+import threading
+import string
+
+load_dotenv()
+client = OpenAI()
+
+with open('scraping/combined_collegeresults_data.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+post_ids = list(data.keys())
+split_data = [post_ids[i:i+62] for i in range(0, len(post_ids), 62)]
+
+output = open('categorization/output.json', 'w')
+prompt = open('categorization/prompt.txt', 'r')
+
+prompt = prompt.readlines()
+prompt = "\n".join(prompt)
+
+result = []
+
+def categorize():
+    for subarray in split_data:
+        for post_id in subarray:
+            completion = client.chat.completions.create(
+                model="gpt-4o",
+                messages=[
+                    {"role": "system", "content": "You are an expert in categorization and objectivity."},
+                    {"role": "user", "content": prompt + "\n\n Here is the student stats\n" + data[post_id]["link_flair_text"] + "\n" + data[post_id]["selftext"]}
+                ],
+                temperature=0.0,
+                seed=42
+            )
+            message = completion.choices[0].message.content
+            message = json.loads(message)
+            result.append(message)
+            print("Processed post id: " + post_id)
+
+categorize()
+output.write(json.dumps(result))
